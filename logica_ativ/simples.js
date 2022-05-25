7f53c964c73f83c6455196a6a98beb333dfbe159
Guilherme Sousa <guilherme.tec1107@gmail.com>
2022-05-25 17:08:24 -0300

    more activities

--- logica_ativ/simples.js
+++ logica_ativ/simples.js
@@ -1,10 +1,17 @@
-//Escreva um programa que pede o raio de um círculo, e em seguida exiba o perímetro e área do círculo.
+const readline = require('readline')
 
-document.getElementById("answer1").addEventListener("click", calc1)
+const client = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-function calc1() {
 
-    const valorRaio = document.getElementById("text1").value
+//  Escreva um programa que pede o raio de um círculo, e em seguida exiba o perímetro e área do círculo.
+
+
+function calc1(x) {
+
+    const valorRaio = x
 
     let p = 2 * Math.PI * valorRaio
 
@@ -13,30 +20,100 @@ function calc1() {
 }
 
 
-// tranformando fahrenheit em graus
+//  tranformando fahrenheit em graus
 
-document.getElementById("answer2").addEventListener("click", calc2)
 
-function calc2(){
+function calc2(x) {
 
-    const valorC = document.getElementById("text2").value
-
-    const valorF = (9 * valorC / 5) * valorC + 32 
+    const valorC = x
+    const valorF = (9 * valorC / 5) * valorC + 32
 
     console.log(valorF, "fahrenheit")
 }
 
 
-// tranformando graus em fahrenheit 
+//  tranformando graus em fahrenheit 
 
-document.getElementById("answer3").addEventListener("click", calc3)
 
-function calc3(){
+function calc3(x) {
 
-    const valorF = document.getElementById("text3").value
-
-    const valorC = 5*(valorF - 32) / 9
+    const valorF = x
+    const valorC = 5 * (valorF - 32) / 9
 
     console.log(valorC, "fahrenheit")
 
 }
+
+//  Crie uma função que recebe um array de 2 números e coloque eles em ordem crescente. Não use o método sort.
+
+function calc4(x) {
+
+    if (x[0] < x[1]) {
+
+        console.log(x)
+    } else {
+
+        let temp = [...x]
+        x[0] = x[1]
+        x[1] = temp[0]
+        console.log(x)
+    }
+}
+
+//Crie uma função que recebe um array de 3 números e coloque eles em ordem crescente. Crie sua própria função para isso.
+
+function calc5(x) {
+
+    x.sort((a, b) => { return a - b })
+    console.log(x)
+
+}
+
+function calc6() {
+
+    client.question("give to me 3 numbers", (threeNum) => {
+        const x = threeNum.split('')
+        for (let i = 0; i < x.length; i++) {
+            const e = Number(x[i])
+            x[i] = e
+
+        }
+
+        x.reverse()
+        console.log(x)
+        client.close()
+
+    })
+}
+
+/*Refaça o script anterior, mas agora pergunte ao usuário quantos números ele deseja inserir no array.
+Depois, o usuário vai preenchendo elemento por elemento do vetor.
+Ao final, exiba o array e o array ao contrário (generalização do script anterior)*/
+
+function calc7() {
+
+    client.question("how many numbers do you want in your array? \n", (answer) => {
+
+        let num = Array(Number(answer)).fill(1)
+
+        console.log(num)
+
+        client.close()
+
+    })
+
+
+    function questions(num){
+
+        console.log(num)
+
+        for (let i = 0; i < num.length; i++) {
+            const e = num[i];
+            client.question(`what is your ${num[i].indexOf}° element?`)
+        }
+    }
+
+
+}
+
+calc7()
